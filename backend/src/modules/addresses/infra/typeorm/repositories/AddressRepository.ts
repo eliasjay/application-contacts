6c0173e8e59b273632f18ba83efbc719fb055392
eliasjay <zeliasgl@gmail.com>
2020-12-23 16:09:34 -0300

    implementing new search methods in address repository

--- backend/src/modules/addresses/infra/typeorm/repositories/AddressRepository.ts
+++ backend/src/modules/addresses/infra/typeorm/repositories/AddressRepository.ts
@@ -10,6 +10,12 @@ class AddressRepository implements IAddressRepository {
   constructor() {
     this.ormRepository = getRepository(Address)
   }
+
+  public async findById(id: string): Promise<Address | undefined> {
+    const address = await this.ormRepository.findOne(id)
+
+    return address
+  }
   
   public async findByUser(user_id: string): Promise<Address[] | undefined> {
     const addresses = await this.ormRepository.find({
--- backend/src/modules/addresses/repositories/IAddressRepository.ts
+++ backend/src/modules/addresses/repositories/IAddressRepository.ts
@@ -1,5 +1,6 @@
 import Address from "../infra/typeorm/entities/Address";
 
 export default interface IAddressRepository {
+  findById(id: string): Promise<Address | undefined>
   findByUser(user_id: string): Promise<Address[] | undefined>
 }
